b6a91a8c437051aada355c26d3a6e3f629ac781b
MrHeyy <Mr.Hey9@gmail.com>
2024-10-30 15:57:19 +0100

    moved assets in corressponding folder

--- Ball/Assets/Scripts/Menu.cs
+++ Ball/Assets/Scripts/Menu.cs
@@ -9,17 +9,37 @@ public class Menu : MonoBehaviour
     public TextMeshProUGUI highScoreText2;
     private float highscore; // Contains the highscore to show on the menu screen.
 
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         highscore = PlayerPrefs.GetFloat("highscore", highscore);
-        highScoreText2.text = "Highscore: " + highscore; // Shows the highscore on the menu screen
+        highScoreText2.text = "Highscore: " + highscore; // Shows the highscore on the menu screen.
+
+        //Gets the gameManager if it exists.
+        GameObject gameManagerObject = GameObject.Find("Game Manager");
+        if (gameManagerObject != null)
+        {
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //Restarts the game if space is clicked.
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            if (gameManager == null)
+            {
+                Play();
+            }
+            else if (!gameManager.isGameActive)
+            {
+                ReturnToMenu();
+            }
+        }
     }
 
     public void Play() // Function for the play button.
