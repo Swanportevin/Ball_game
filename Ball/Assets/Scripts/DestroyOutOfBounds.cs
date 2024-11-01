6129c52c4fbc312e5354171f64b13b2579b29613
Swanportevin <swanportevin@gmail.com>
2024-11-01 15:31:07 +0100

    Update to destroy pipe
    lowered the behindBound value from -50 to -70, due to the halfpipes getting deleted too soon behind the camera.

--- Ball/Assets/Scripts/DestroyOutOfBounds.cs
+++ Ball/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float behindBound = -50.0f; // Point behind the camera at which the objects get destroyed.
+    private float behindBound = -70.0f; // Point behind the camera at which the objects get destroyed.
     private float BottomLimit = -20.0f; // Point under the halfpipes at which falling objects get destroyed.
 
     private GameManager GameManager_script;
